2ed7ddca461c70ee35378a68a63292e4ce54c417
KerenBermeo <bermeokeren@gmail.com>
2024-12-03 22:04:13 -0500

    tengo data-bs-dismiss por lo que quite el cierre del modal desde js

--- script.js
+++ script.js
@@ -8,14 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
         openModalButton.addEventListener('click', function () {
             modal.show(); // Mostrar el modal cuando se hace clic en el botón
         });
+        // Realizar la petición al archivo PHP
     }
 
-    // También puedes añadir el evento para el botón de cerrar el modal (esto es opcional si usas el data-bs-dismiss)
-    const closeModalButton = document.getElementById('closeModal');
-    if (closeModalButton) {
-        closeModalButton.addEventListener('click', function () {
-            modal.hide(); // Cerrar el modal si haces clic en "Cancelar"
-        });
-    }
 });
 
